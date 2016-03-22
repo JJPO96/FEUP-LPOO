37c8de8875a3c88e803bbfa630aa25f05ebc7c02
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2016-03-21 23:37:43 -0100

    GUI improved

--- Maze/src/maze/gui/GameLauncherGUI.java
+++ Maze/src/maze/gui/GameLauncherGUI.java
@@ -21,15 +21,85 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import maze.logic.*;
+import maze.logic.Maze.Direction;
+import maze.logic.Maze.Mode;
+
 import javax.swing.JTextArea;
+
+
 
 public class GameLauncherGUI {
 
 	private JFrame frmTitulo;
 	private final Action action = new SwingAction();
-	private JTextField textFieldDimensoes;
-	private JTextField textFieldNrDragoes;
-
+	private JTextField textDim;
+	private JTextField textNrDrag;
+	private Maze maze;
+	private int drAlive = 0;
+	private int dragons;
+	
+	
+	/**
+	 *Direction buttons auxiliary function
+	 * Input Values:
+	 * 	0 - up
+	 * 	1 - down 
+	 * 	2 - left 
+	 * 	3 - right
+	 * 
+	 * Output values
+	 * 	0 - error //
+	 * 	1 - hero moved, nothing happened //
+	 *  2 - hero/dragon moved, hero is dead //
+	 *  3 - hero moved, is armed //
+	 *  4 - hero moved, killed a dragon //
+	 *  5 - hero moved, killed all dragons, exit open //
+	 *  6 - hero exited the maze //
+	 */
+	
+	public int auxButton(int button,int drAlive){ 
+		switch (button){
+		case 0:
+			maze.update(Direction.UP);
+			break;
+		case 1:
+			maze.update(Direction.DOWN);
+			break;
+		case 2:
+			maze.update(Direction.LEFT);
+			break;
+		case 3:
+			maze.update(Direction.RIGHT);
+			break;
+		}
+		int tempDrAlive = 0;
+		for(Dragon dr : maze.getDragons())
+			if(dr.isAlive())
+				tempDrAlive++;
+	
+		if(maze.hasDragonsAlive()){
+			 if(maze.getHero().isAlive()){
+				 if(maze.getHero().isArmed())
+					 return 3;
+				 else if(tempDrAlive < drAlive)
+					 return 4;
+				 else
+					 return 1;
+			 }else
+				 return 2;
+				 
+		}else{
+			if(maze.getHero().getPos().getX() == maze.getGameBoard().getExitPos().getX() && maze.getHero().getPos().getY() == maze.getGameBoard().getExitPos().getY())
+				return 6;
+			else
+				return 5;
+		}
+	}
+	
 	/**
 	 * Launch the application.
 	 */
@@ -48,8 +118,13 @@ public class GameLauncherGUI {
 
 	/**
 	 * Create the application.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public GameLauncherGUI() {
+	public GameLauncherGUI() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		initialize();
 	}
 
@@ -58,99 +133,141 @@ public class GameLauncherGUI {
 	 */
 	private void initialize() {
 		frmTitulo = new JFrame();
-		frmTitulo.setTitle("Jogo do Labirinto");
+		frmTitulo.setTitle("Maze");
 		frmTitulo.setResizable(false);
-		frmTitulo.setBounds(100, 100, 629, 358);
+		frmTitulo.setBounds(100, 100, 630, 400);
 		frmTitulo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTitulo.getContentPane().setLayout(null);
 		
-		JLabel lblDimensoDoLabirinto = new JLabel("Dimens\u00E3o do Labirinto");
-		lblDimensoDoLabirinto.setBounds(35, 25, 135, 25);
-		frmTitulo.getContentPane().add(lblDimensoDoLabirinto);
+		//local variables
 		
-		JLabel label = new JLabel("N\u00FAmero de Drag\u00F5es");
-		label.setBounds(35, 65, 135, 25);
-		frmTitulo.getContentPane().add(label);
 		
-		JLabel lblTipoDeDrages = new JLabel("Tipo de Drag\u00F5es");
-		lblTipoDeDrages.setBounds(35, 105, 135, 25);
-		frmTitulo.getContentPane().add(lblTipoDeDrages);
+		
+		// Buttons and fields declarations
+		JLabel lblMazeDim = new JLabel("Maze Dimension");
+		lblMazeDim.setBounds(35, 25, 135, 25);
+		frmTitulo.getContentPane().add(lblMazeDim);
+		
+		JLabel lblNumberDragons = new JLabel("Number of Dragons");
+		lblNumberDragons.setBounds(35, 65, 135, 25);
+		frmTitulo.getContentPane().add(lblNumberDragons);
+		
+		JLabel lblTypeDrag = new JLabel("Type of Dragons");
+		lblTypeDrag.setBounds(35, 105, 135, 25);
+		frmTitulo.getContentPane().add(lblTypeDrag);
 		
 		JLabel lblGameState = new JLabel("Crie o seu labirinto.");
-		lblGameState.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGameState.setBounds(386, 287, 220, 20);
+		lblGameState.setHorizontalAlignment(SwingConstants.CENTER);
 		frmTitulo.getContentPane().add(lblGameState);
 		
-		textFieldDimensoes = new JTextField();
-		textFieldDimensoes.setHorizontalAlignment(SwingConstants.CENTER);
-		textFieldDimensoes.setText("11");
-		textFieldDimensoes.setBounds(175, 25, 110, 20);
-		frmTitulo.getContentPane().add(textFieldDimensoes);
-		textFieldDimensoes.setColumns(10);
+		textDim = new JTextField();
+		textDim.setBounds(175, 25, 125, 20);
+		textDim.setHorizontalAlignment(SwingConstants.CENTER);
+		textDim.setText("11");
+		frmTitulo.getContentPane().add(textDim);
+		textDim.setColumns(10);
 		
-		textFieldNrDragoes = new JTextField();
-		textFieldNrDragoes.setHorizontalAlignment(SwingConstants.CENTER);
-		textFieldNrDragoes.setText("1");
-		textFieldNrDragoes.setColumns(10);
-		textFieldNrDragoes.setBounds(175, 65, 110, 20);
-		frmTitulo.getContentPane().add(textFieldNrDragoes);
+		textNrDrag = new JTextField();
+		textNrDrag.setBounds(175, 65, 125, 20);
+		textNrDrag.setHorizontalAlignment(SwingConstants.CENTER);
+		textNrDrag.setText("1");
+		textNrDrag.setColumns(10);
+		frmTitulo.getContentPane().add(textNrDrag);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.addItem("Imóveis");
-		comboBox.addItem("Móveis");
-		comboBox.addItem("Móveis c/ sono");
-		comboBox.setBounds(175, 105, 110, 20);
-		frmTitulo.getContentPane().add(comboBox);
+		JComboBox modeSlct = new JComboBox();
+		modeSlct.setBounds(175, 105, 125, 20);
+		modeSlct.addItem("Static");
+		modeSlct.addItem("Moving");
+		modeSlct.addItem("Moving and Sleepy");
+		frmTitulo.getContentPane().add(modeSlct);
 		
 		JTextArea textArea = new JTextArea();
+		textArea.setBounds(35, 136, 341, 215);
 		textArea.setEditable(false);
-		textArea.setBounds(35, 136, 341, 182);
 		frmTitulo.getContentPane().add(textArea);
 		
-		JButton btnSair = new JButton("Sair");
-		btnSair.addActionListener(new ActionListener() {
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(420, 80, 150, 35);
+		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-		btnSair.setBounds(425, 80, 150, 35);
-		frmTitulo.getContentPane().add(btnSair);
+		frmTitulo.getContentPane().add(btnExit);
 		
-		JButton btnCima = new JButton("CIMA");
-		btnCima.setEnabled(false);
-		btnCima.setBounds(445, 180, 100, 25);
-		frmTitulo.getContentPane().add(btnCima);
-		
-		JButton btnBaixo = new JButton("BAIXO");
-		btnBaixo.setEnabled(false);
-		btnBaixo.addActionListener(new ActionListener() {
+		JButton btnUp = new JButton("UP");
+		btnUp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				auxButton(0,drAlive);
+				textArea.setText(maze.toString());
 			}
 		});
-		btnBaixo.setBounds(445, 250, 100, 25);
-		frmTitulo.getContentPane().add(btnBaixo);
+		btnUp.setBounds(445, 180, 100, 25);
+		btnUp.setEnabled(false);
+		frmTitulo.getContentPane().add(btnUp);
 		
-		JButton btnEsq = new JButton("ESQUERDA");
-		btnEsq.setEnabled(false);
-		btnEsq.setBounds(385, 215, 100, 25);
-		frmTitulo.getContentPane().add(btnEsq);
-		
-		JButton btnDir = new JButton("DIREITA");
-		btnDir.setEnabled(false);
-		btnDir.setBounds(505, 215, 100, 25);
-		frmTitulo.getContentPane().add(btnDir);
-		
-		JButton btnGerarNovoLabirinto = new JButton("Gerar novo labirinto");
-		btnGerarNovoLabirinto.addActionListener(new ActionListener() {
+		JButton btnDown = new JButton("DOWN");
+		btnDown.setBounds(445, 250, 100, 25);
+		btnDown.setEnabled(false);
+		btnDown.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				btnCima.setEnabled(true);
-				btnBaixo.setEnabled(true);
-				btnEsq.setEnabled(true);
-				btnDir.setEnabled(true);
+				auxButton(1,drAlive);
+				textArea.setText(maze.toString());
 			}
 		});
-		btnGerarNovoLabirinto.setBounds(425, 35, 150, 35);
-		frmTitulo.getContentPane().add(btnGerarNovoLabirinto);
+		frmTitulo.getContentPane().add(btnDown);
+		
+		JButton btnLeft = new JButton("LEFT");
+		btnLeft.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				auxButton(2,drAlive);
+				textArea.setText(maze.toString());
+			}
+		});
+		btnLeft.setBounds(385, 215, 100, 25);
+		btnLeft.setEnabled(false);
+		frmTitulo.getContentPane().add(btnLeft);
+		
+		JButton btnRight = new JButton("RIGHT");
+		btnRight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				auxButton(3,drAlive);
+				textArea.setText(maze.toString());
+			}
+		});
+		btnRight.setBounds(505, 215, 100, 25);
+		btnRight.setEnabled(false);
+		frmTitulo.getContentPane().add(btnRight);
+		
+		JButton btnNewMaze = new JButton("New Maze");
+		btnNewMaze.setBounds(420, 35, 150, 35);
+		btnNewMaze.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO ver maus inputs
+				Mode mode = null;
+				String strMode = modeSlct.getSelectedItem().toString();
+				if(strMode == "Static")
+					mode = Mode.STATIC;
+				else if(strMode == "Moving")
+					mode = Mode.MOVING;
+				else if(strMode == "Moving and Sleepy")
+					mode = Mode.MOVINGSLEEPING;
+				
+				int dim = Integer.parseInt(textDim.getText());
+				dragons = Integer.parseInt(textNrDrag.getText());
+				drAlive = dragons;
+				maze = new Maze(mode,dragons,dim);
+				textArea.setText(maze.toString());
+				btnUp.setEnabled(true);
+				btnDown.setEnabled(true);
+				btnLeft.setEnabled(true);
+				btnRight.setEnabled(true);
+				lblGameState.setText("Game Running");
+				
+			}
+		});
+		frmTitulo.getContentPane().add(btnNewMaze);
 
 	}
 	private class SwingAction extends AbstractAction {
