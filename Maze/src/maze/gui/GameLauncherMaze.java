ad72f12b9d83a639a505e5e4001f9a70e7cff890
MaggGomes <magg.slb@gmail.cm>
2016-04-04 19:35:02 +0100

    no message

--- Maze/src/maze/gui/GameLauncherMaze.java
+++ Maze/src/maze/gui/GameLauncherMaze.java
@@ -73,8 +73,8 @@ public class GameLauncherMaze {
 		frmDragonsMaze.setBackground(Color.WHITE);
 		frmDragonsMaze.setIconImage(Toolkit.getDefaultToolkit().getImage(GameLauncherMaze.class.getResource("/maze/gui/res/dragon.png")));
 		frmDragonsMaze.setTitle("Dragon's Maze");
-		frmDragonsMaze.setBounds(0, 0, 600, 670);
-		frmDragonsMaze.setPreferredSize(new Dimension(600, 670));		
+		frmDragonsMaze.setBounds(0, 0, 600, 660);
+		frmDragonsMaze.setPreferredSize(new Dimension(600, 660));		
 		frmDragonsMaze.setResizable(false);
 		frmDragonsMaze.setLocationRelativeTo(null);
 		frmDragonsMaze.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -207,7 +207,7 @@ public class GameLauncherMaze {
 			}
 		});
 		btnExit.setFont(new Font("Tempus Sans ITC", Font.BOLD, 9));
-		btnExit.setBounds(518, 0, 85, 40);
+		btnExit.setBounds(518, 0, 76, 40);
 		frmDragonsMaze.getContentPane().add(btnExit);		
 
 		frmDragonsMaze.pack();		
