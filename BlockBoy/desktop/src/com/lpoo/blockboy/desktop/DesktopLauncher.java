71f7d47634b664eac9292e7d5da82e203238dbb0
MaggGomes <magg.slb@gmail.cm>
2016-05-11 17:43:53 +0100

    first commit done

---
+++ BlockBoy/desktop/src/com/lpoo/blockboy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lpoo.blockboy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lpoo.blockboy.BlockBoy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlockBoy(), config);
+	}
+}
