1b8fec96ba3314eb9cff17bb548c66dcfe14e9b5
MaggGomes <magg.slb@gmail.cm>
2016-06-01 01:59:30 +0100

    no message

--- BlockBoy/core/src/com/lpoo/blockboy/gui/Hud.java
+++ BlockBoy/core/src/com/lpoo/blockboy/gui/Hud.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -26,7 +28,9 @@ public class Hud implements Disposable{
     private Label countdownLabel;
     private Label timeLabel;
     private Label spacing;
-    private Table table;
+    private Table tableTop;
+    private Table tableBottom;
+    private TextButton buttonBox;
 
     // TODO - FALTA TERMINAR
     public Hud(SpriteBatch batch, BitmapFont font){
@@ -35,24 +39,29 @@ public class Hud implements Disposable{
         this.timeCount = 0;
         this.viewport = new FitViewport(BlockBoy.VHEIGHT, BlockBoy.VHEIGHT, new OrthographicCamera());
         this.stage = new Stage(viewport, batch);
-        this.table = new Table();
-        this.table.top();
-        this.table.setFillParent(true);
+        this.tableTop = new Table();
+        this.tableTop.top();
+        this.tableTop.setFillParent(true);
+        this.tableBottom = new Table();
+        this.tableBottom.bottom();
+        this.tableBottom.setFillParent(true);
         this.countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(font, Color.WHITE));
         this.timeLabel = new Label("TIME", new Label.LabelStyle(font, Color.WHITE));
         this.coinsCount = new Label(String.format("%01d", numCoins), new Label.LabelStyle(font, Color.WHITE));
         this.coinsLabel = new Label("COINS", new Label.LabelStyle(font, Color.WHITE));
         this.spacing = new Label("", new Label.LabelStyle(font, Color.WHITE));
+        //this.buttonBox.add("Pick Box", TextButton.TextButtonStyle());
 
-        table.add(coinsLabel).expandX();
-        table.add(coinsCount).expandX();
-        table.add(spacing).expandX();
-        table.add(spacing).expandX();
-        table.add(spacing).expandX();
-        table.add(spacing).expandX();
-        table.add(timeLabel).expandX();
-        table.add(countdownLabel).expandX();
-        stage.addActor(table);
+        tableTop.add(coinsLabel).expandX();
+        tableTop.add(coinsCount).expandX();
+        tableTop.add(spacing).expandX();
+        tableTop.add(spacing).expandX();
+        tableTop.add(spacing).expandX();
+        tableTop.add(spacing).expandX();
+        tableTop.add(timeLabel).expandX();
+        tableTop.add(countdownLabel).expandX();
+        stage.addActor(tableTop);
+        stage.addActor(tableBottom);
     }
 
     public Stage getStage() {
