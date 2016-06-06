f3b78137c9626206de09361560c9396bd37cda45
José Oliveira <Jos Oliveira>
2016-06-06 22:04:54 +0100

    tests folders updated

---
+++ BlockBoy/tests/src/logic/BlockBoyTester.java
@@ -0,0 +1,17 @@
+package logic;
+
+import com.lpoo.blockboy.logic.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+@RunWith(GdxTestRunner.class)
+public class BlockBoyTester {
+
+}
